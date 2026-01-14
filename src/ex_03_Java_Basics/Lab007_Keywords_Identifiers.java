82668f5a9a7ccd52660face870372809533686e0
Mohana Priya <mohanapriya765@gmail.com>
2026-01-14 20:02:40 +0530

    Ex3

---
+++ src/ex_03_Java_Basics/Lab007_Keywords_Identifiers.java
@@ -0,0 +1,23 @@
+package ex_03_Java_Basics;
+
+public class Lab007_Keywords_Identifiers {
+    public static void main(String[] args) {
+        String a = "Java";
+        String b = new String("Java");
+
+        System.out.println(a == b);      // false
+        System.out.println(a.equals(b));
+
+        String s = "Ja" + "va";
+        System.out.println(s);
+        System.out.println(s+a+b);
+
+
+        System.out.println(2+2);
+        System.out.println("2+2");
+        System.out.println(0/10);
+
+
+    }
+}
+
